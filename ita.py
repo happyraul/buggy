1b6efbbb792473141a53e46317143ae802e447e2
solcld <happyraul@users.noreply.github.com>
2018-02-25 20:13:26 +0100

    parse outbound, return legs

--- ita.py
+++ ita.py
@@ -1,3 +1,4 @@
+import re as _re
 
 class _sel():
     from selenium.webdriver import Firefox
@@ -15,14 +16,16 @@ def get_driver():
     return driver
 
 
-def search(origin, destination, departure_date, return_date):
-    print('Creating driver...')
-    driver = get_driver()
+def search(origin, destination, departure_date, return_date, driver=None):
+    if driver is None:
+        print('Creating driver...')
+        driver = get_driver()
+
     url = 'https://matrix.itasoftware.com/'
-    print(f'Loading {url}...')
+    print(f'Loading `{url}`...')
     driver.get(url)
 
-    send_keys(driver, 'cityPair-orig-0', origin, timeout=5)
+    send_keys(driver, 'cityPair-orig-0', origin, timeout=6)
     click_suggestion(driver, origin)
     send_keys(driver, 'cityPair-dest-0', destination)
     click_suggestion(driver, destination)
@@ -31,16 +34,102 @@ def search(origin, destination, departure_date, return_date):
     send_keys(driver, 'cityPair-retDate-0', return_date)
 
     driver.find_element_by_id('searchButton-0').click()
-    print(f'Searching for {origin} to {destination} - '
-          f'departing {departure_date}, returning {return_date}...')
+    print(f'Searching for `{origin}` to `{destination}` - '
+          f'departing `{departure_date}`, returning `{return_date}`...')
 
+    # driver.get_screenshot_as_file('001-search-begin.png')
     wait = _sel.WebDriverWait(driver, timeout=60)
     wait.until(lambda driver: not _sel.expected.visibility_of_element_located(
-        (_sel.By.CLASS_NAME, 'IR6M2QD-n-c')
+        (_sel.By.XPATH, '//div[contains(text(), "Searching for flights")]')
     )(driver))
-    driver.get_screenshot_as_file('01-search-end.png')
+    # driver.get_screenshot_as_file('002-search-end.png')
 
-    return driver
+    details = []
+    divs = []
+    # buttons = get_buttons(driver)
+    for idx in range(1):
+        # buttons = get_buttons(driver)
+        button = get_buttons(driver)[idx]
+        price = button.text
+        print(f'Retrieving itinerary details for `{price}`...')
+        button.click()
+
+        wait.until(lambda driver: not _sel.expected.visibility_of_element_located(
+            (_sel.By.XPATH, '//div[contains(text(), "Retrieving itinerary details")]')
+        )(driver))
+        driver.get_screenshot_as_file(f'003-{idx}-details.png')
+        # return driver, '', ''
+        div, parsed = parse_details(driver)
+        parsed['price'] = price
+        details.append(parsed)
+        divs.append(div)
+        driver.back()
+
+        wait.until(lambda driver: not _sel.expected.visibility_of_element_located(
+            (_sel.By.XPATH, '//div[contains(text(), "Updating flight info")]')
+        )(driver))
+    return driver, divs, details
+
+
+def get_buttons(driver):
+    return [button
+            for button in driver.find_elements_by_xpath('//div/button/span') 
+            if button.text]
+
+
+def parse_details(driver):
+    details_div = driver.find_element_by_xpath('//div[contains(text(), "Itinerary")]/following-sibling::div')
+    out, return_ = map(parse_leg, details_div.find_elements_by_tag_name('table'))
+    return details_div, {'out': out, 'return': return_}
+
+
+def parse_leg(leg):
+    pattern = _re.compile('^.+\((?P<origin>[A-Z]{3})\).+\((?P<destination>[A-Z]{3})\)')
+    rows = leg.find_elements_by_tag_name('tr')[1:]
+    segments = []
+    for segment in [iter(rows[i:i + 3]) for i in range(0, len(rows), 3)]:
+        route = next(segment).find_element_by_tag_name('div').text
+        match = _re.search(pattern, route)
+        data = {}
+        if match:
+            data['origin'] = match.group('origin')
+            data['destination'] = match.group('destination')
+
+        data.update(parse_schedule(next(segment)))
+        extra = next(segment, None)
+        if extra and is_layover(extra.text):
+            data['layover'] = parse_layover(extra)
+        segments.append(data)
+    return segments 
+
+
+def parse_schedule(row):
+    flight, depart, arrive, duration, aircraft, booking_code = [
+        td.text for td in row.find_elements_by_tag_name('td') if td.text
+    ]
+    return dict(
+        flight=flight,
+        depart=depart.split('Dep: ')[1],
+        arrive=arrive.split('Arr: ')[1],
+        duration=duration,
+        aircraft=aircraft, 
+        booking_code=booking_code,
+    )
+
+def parse_layover(row):
+    layover = iter([td.text for td in row.find_elements_by_tag_name('td') if td.text])
+    data = next(layover)
+    while data:
+        if is_layover(data):
+            key = data.split('Layover in ')[1]
+        else:
+            value = data
+        data = next(layover, None)
+    return {key: value}
+
+
+def is_layover(row):
+    return row and 'Layover' in row
 
 
 def send_keys(driver, id_, value, timeout=None):
@@ -66,7 +155,9 @@ def click_suggestion(driver, input_):
             break
 
 if __name__ == "__main__":
-    search('BOS', 'AMS', '03/14/2018', '03/25/2018')
+    import pprint as _pp
+    _, _, details = search('BOS', 'FRA', '03/14/2018', '03/25/2018')
+    _pp.pprint(details)
     # options = _sel.Options()
     # options.add_argument('-headless')
     # driver = _sel.Firefox(executable_path='geckodriver', firefox_options=options)
