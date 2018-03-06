2d14f05d3eee2be64f487b40e30b51fbff6b5267
Raul Taranu <597630+happyraul@users.noreply.github.com>
2018-03-06 19:14:08 +0100

    parse duration, prices, add best_price, filter out bad itineraries

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 
+import datetime as _dt
+import decimal as _decimal
 import functools as _ft
+import operator as _op
 
 import common as _common
 
@@ -11,7 +14,20 @@ class _sel():
     from selenium.webdriver.support.wait import WebDriverWait
 
 
-def search(origin, destination, departure_date=None, driver=None):
+destinations = [
+    # ('Berlin', _dt.timedelta(seconds=60 * 60 * 5)),
+    ('Aachen', _dt.timedelta(seconds=60 * 60 * 6)),
+    # ('Hamburg', _dt.timedelta(seconds=60 * 60 * 7)),
+]
+
+travel_dates = [
+    # '08.04.2018',
+    '15.04.2018',
+]
+
+
+def search(origin, destination, departure_date=None, driver=None, best=False,
+           duration_limit=None):
     if driver is None:
         print('Creating driver...')
         driver = _common.get_chrome_driver()
@@ -34,7 +50,7 @@ def search(origin, destination, departure_date=None, driver=None):
         driver.get_screenshot_as_file('000-db-back.png')
         send_keys('14:00', name='time')
 
-    print('Submitting...')
+    print(f'Submitting search for `{destination}`...')
     driver.get_screenshot_as_file('001-db.png')
     driver.find_element_by_class_name('js-submit-btn').click()
     driver.get_screenshot_as_file('002-db.png')
@@ -56,14 +72,64 @@ def search(origin, destination, departure_date=None, driver=None):
 
     results = []
     for result in driver.find_elements_by_class_name('boxShadow'):
-        results.append(dict(
-            departure_time=result.find_element_by_xpath(
-                "tr[@class='firstrow']/td[@class='time']").text.strip(),
-            duration=result.find_element_by_xpath(
-                "tr[@class='firstrow']/td[contains(@class, 'duration') and "
-                "contains(@class, 'lastrow')]"
-            ).text.strip(),
-            price=result.find_element_by_class_name('fareOutput').text.strip()
-        ))
+        if has_price(result):
+            results.append(dict(
+                departure_time=result.find_element_by_xpath(
+                    "tr[@class='firstrow']/td[@class='time']").text.strip(),
+                duration=parse_duration(result),
+                price=parse_price(result)
+            ))
+
+    if duration_limit:
+        results = [result for result in results
+                   if result['duration'] < duration_limit]
+
+    # FIXME: how to fix the duration so its readable
+    # results = [
+    #     dict(**result, duration=str(result['duration'])) for result in results
+    # ]
+
+    if best:
+        results = sorted(results, key=_op.itemgetter('price'))[0]
 
     return driver, results
+
+
+def parse_duration(result):
+    duration = result.find_element_by_xpath(
+        "tr[@class='firstrow']/td[contains(@class, 'duration') "
+        "and contains(@class, 'lastrow')]"
+    ).text.strip()
+    hours, minutes = map(int, duration.split(':'))
+    return _dt.timedelta(seconds=60 * 60 * hours + 60 * minutes)
+
+
+def parse_price(result):
+    prices = [
+        price.text.strip() for price in
+        result.find_elements_by_class_name('fareOutput')
+    ]
+    print()
+    return min(
+        _decimal.Decimal(price.split(' EUR')[0].replace(',', '.'))
+        for price in prices
+    )
+
+
+def has_price(result):
+    return bool(result.find_elements_by_class_name('fareOutput'))
+
+
+def best_price():
+    results = {}
+    driver = _common.get_chrome_driver()
+    for destination, duration_limit in destinations:
+        for date in travel_dates:
+            _, result = search('Munich', destination, departure_date=date,
+                               best=True, driver=driver,
+                               duration_limit=duration_limit)
+            if destination not in results or \
+                    results[destination]['price'] > result['price']:
+                results[destination] = result
+                results[destination]['date'] = date
+    return results
