ee70a8f7cef196a3bec1b5da583dc172ccbb888d
Julie MacDonell <julie.macdonell@stylight.com>
2018-03-22 18:12:05 +0100

    Outbound limits

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import datetime as _dt
 import decimal as _decimal
 import functools as _ft
 import operator as _op
+import typing as _typing
 
 import common as _common
 
@@ -14,20 +15,46 @@ class _sel():
     from selenium.webdriver.support.wait import WebDriverWait
 
 
-destinations = [
-    # ('Berlin', _dt.timedelta(seconds=60 * 60 * 5)),
-    ('Aachen', _dt.timedelta(seconds=60 * 60 * 6)),
-    # ('Hamburg', _dt.timedelta(seconds=60 * 60 * 7)),
+# destinations = [
+#     # ('Berlin', _dt.timedelta(seconds=60 * 60 * 5)),
+#     ('Aachen', _dt.timedelta(seconds=60 * 60 * 6), ),
+#     # ('Hamburg', _dt.timedelta(seconds=60 * 60 * 7)),
+# ]
+
+
+class Query(_typing.NamedTuple):
+    destination: str
+    departure_date: str
+    duration_limit: _dt.timedelta = None
+    origin: str = 'Munich'
+    outbound_start: _dt.time = None
+    latest_arrival: _dt.time = None
+
+
+queries = [
+    Query(
+        destination='Aachen', departure_date='15.04.2018',
+        duration_limit=_dt.timedelta(seconds=60 * 60 * 6),
+        outbound_start=_dt.time(hour=7), latest_arrival=_dt.time(hour=18)
+    )
 ]
 
-travel_dates = [
-    # '08.04.2018',
-    '15.04.2018',
-]
+
+# travel_dates = [
+#     # '08.04.2018',
+#     '15.04.2018',
+# ]
+
+# class Duration(_dt.timedelta):
+#
+#     def __new__(cls, *args, **kwargs):
+#         _dt.timedelta.
+#
+#     def __repr__(self):
+#         return str(self)
 
 
-def search(origin, destination, departure_date=None, driver=None, best=False,
-           duration_limit=None):
+def search(query, driver=None, best=False):
     if driver is None:
         print('Creating driver...')
         driver = _common.get_chrome_driver()
@@ -36,21 +63,22 @@ def search(origin, destination, departure_date=None, driver=None, best=False,
 
     print('Getting bahn.de...')
     driver.get('https://bahn.de')
-    send_keys(origin, id_='js-auskunft-autocomplete-from', timeout=6)
-    send_keys(destination, id_='js-auskunft-autocomplete-to')
+    send_keys(query.origin, id_='js-auskunft-autocomplete-from', timeout=6)
+    send_keys(query.destination, id_='js-auskunft-autocomplete-to')
     driver.find_element_by_id('0').click()
 
-    if departure_date:
-        send_keys(departure_date, name='date', clear=True)
+    if query.departure_date:
+        send_keys(query.departure_date, name='date', clear=True)
         driver.get_screenshot_as_file('000-db.png')
         driver.find_element_by_id('js-auskunft-autocomplete-from').click()
         time_field = driver.find_element_by_name('time')
         for _ in range(5):
             time_field.send_keys(_sel.Keys.BACK_SPACE)
         driver.get_screenshot_as_file('000-db-back.png')
-        send_keys('14:00', name='time')
+        outbound_start = query.outbound_start or _dt.time(hour=14)
+        send_keys(outbound_start.strftime('%H:%M'), name='time')
 
-    print(f'Submitting search for `{destination}`...')
+    print(f'Submitting search for `{query.destination}`...')
     driver.get_screenshot_as_file('001-db.png')
     driver.find_element_by_class_name('js-submit-btn').click()
     driver.get_screenshot_as_file('002-db.png')
@@ -76,18 +104,23 @@ def search(origin, destination, departure_date=None, driver=None, best=False,
             results.append(dict(
                 departure_time=result.find_element_by_xpath(
                     "tr[@class='firstrow']/td[@class='time']").text.strip(),
+                arrival_time=result.find_element_by_xpath(
+                    "tr[@class='last']/td[@class='time']").text.strip(),
                 duration=parse_duration(result),
                 price=parse_price(result)
             ))
 
-    if duration_limit:
+    if query.latest_arrival:
         results = [result for result in results
-                   if result['duration'] < duration_limit]
+                   if result['arrival_time'] < query.latest_arrival]
 
-    # FIXME: how to fix the duration so its readable
-    # results = [
-    #     dict(**result, duration=str(result['duration'])) for result in results
-    # ]
+    if query.duration_limit:
+        results = [result for result in results
+                   if result['duration'] < query.duration_limit]
+
+    results = [
+        dict(result, duration=str(result['duration'])) for result in results
+    ]
 
     if best:
         results = sorted(results, key=_op.itemgetter('price'))[0]
@@ -109,7 +142,6 @@ def parse_price(result):
         price.text.strip() for price in
         result.find_elements_by_class_name('fareOutput')
     ]
-    print()
     return min(
         _decimal.Decimal(price.split(' EUR')[0].replace(',', '.'))
         for price in prices
@@ -123,13 +155,14 @@ def has_price(result):
 def best_price():
     results = {}
     driver = _common.get_chrome_driver()
-    for destination, duration_limit in destinations:
-        for date in travel_dates:
-            _, result = search('Munich', destination, departure_date=date,
-                               best=True, driver=driver,
-                               duration_limit=duration_limit)
-            if destination not in results or \
-                    results[destination]['price'] > result['price']:
-                results[destination] = result
-                results[destination]['date'] = date
+    for query in queries:
+        _, result = search(query, best=True, driver=driver)
+        if query.destination not in results or \
+                results[query.destination]['price'] > result['price']:
+            results[query.destination] = result
+            results[query.destination]['date'] = query.departure_date
     return results
+
+
+if __name__ == '__main__':
+    print(best_price())
